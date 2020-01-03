03bdf8159337edb937e9444f2171bf51be99c6ee
ningyunziying <863678717@qq.com>
2020-01-03 22:34:45 +0800

    add kafka

--- spring-boot-kafka/src/main/java/com/ningyunziying/kafka/Consumer.java
+++ spring-boot-kafka/src/main/java/com/ningyunziying/kafka/Consumer.java
@@ -1,4 +1,26 @@
 package com.ningyunziying.kafka;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class Consumer {
+    // 简单消费者
+    @KafkaListener(topics = Topic.SIMPLE)
+    public void consumer1_1(ConsumerRecord<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, Consumer consumer, Acknowledgment ack) {
+        log.info("单独消费者消费消息,topic= {} ,content = {}", topic, record.value());
+        log.info("consumer content = {}", consumer);
+        ack.acknowledge();
+
+        /*
+         * 如果需要手工提交异步 consumer.commitSync();
+         * 手工同步提交 consumer.commitAsync()
+         */
+    }
 }
--- spring-boot-kafka/src/main/java/com/ningyunziying/kafka/KafkaApplication.java
+++ spring-boot-kafka/src/main/java/com/ningyunziying/kafka/KafkaApplication.java
@@ -1,13 +1,17 @@
 package com.ningyunziying.kafka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class KafkaApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Override
+    public void run(String... args) throws Exception {
 
+    }
 }
