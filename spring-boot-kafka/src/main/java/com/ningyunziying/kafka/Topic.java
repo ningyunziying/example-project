a845f370fef71c231a84176320383f041d9362e9
Ningning Li <ningningli@emotibot.com>
2020-01-27 00:01:21 +0800

    remove kafka demo

--- spring-boot-kafka/src/main/java/com/ningyunziying/kafka/Topic.java
+++
@@ -1,7 +0,0 @@
-package com.ningyunziying.kafka;
-
-public class Topic {
-    public static final String GROUP = "newGroup";
-    public static final String SIMPLE = "simple";
-    public static final String BEAN = "spring.boot.kafka.bean";
-}
