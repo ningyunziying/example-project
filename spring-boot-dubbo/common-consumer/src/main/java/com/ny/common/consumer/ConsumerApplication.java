295120ca6b7113f0590e509b29872927ed1f2c82
Ningning Li <ningyunziying@gmail.com>
2020-06-14 23:16:32 +0800

    adfas

--- spring-boot-dubbo/common-consumer/src/main/java/com/ny/common/consumer/ConsumerApplication.java
+++ spring-boot-dubbo/common-consumer/src/main/java/com/ny/common/consumer/ConsumerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.Resource;
-import javax.swing.*;
 
 @SpringBootApplication
 public class ConsumerApplication implements CommandLineRunner {
